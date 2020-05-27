20f7776aaba2cbcde5511d5721293b5586088e42
Jagraj Singh <singhjagraj900@gmail.com>
2020-05-28 00:48:59 +0100

    Further lessons on widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     appBar: AppBar(
-      title: Text('Hello World!'),
+      title: Text('Octopus Energy'),
       centerTitle: true,
-      backgroundColor: Colors.purple[400],
+      backgroundColor: Colors.purple[600],
     ),
-    body: Center(
-      child: Text(
-        'My first app.',
-        style: TextStyle(
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.purple[600],
-          fontFamily: 'Gotham',
+    body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(20.0),
+          color: Colors.amber,
+          child: Text('Container One')
         ),
-      ),
+        Container(
+          padding: EdgeInsets.all(40.0),
+          color: Colors.pink,
+          child: Text('Container One')
+        ),
+        Container(
+          padding: EdgeInsets.all(30.0),
+          color: Colors.green,
+          child: Text('Container One')
+        ),
+      ],
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
       child: Text('Submit'),
-      backgroundColor: Colors.purple[400],
+      backgroundColor: Colors.purple[600],
     ),
-  ),
-));
+  );
+  }
+}
 
